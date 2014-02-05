48c2a9c745e91e5ac5625ba80cf07608521cf4c8
luigi.sk <luigi.sk@gmail.com>
2014-02-05 22:53:52 +0100

    first commit

--- app/mailers/la_department_watcher/department_mailer.rb
+++ app/mailers/la_department_watcher/department_mailer.rb
@@ -5,7 +5,7 @@ module LaDepartmentWatcher
     def notify_alert(department)
       @department = department
       @minutes_offline = (LaDepartmentWatcher::Config.get.config[:notify_offset] * (1 / 60)).to_i
-      mail( to: LaDepartmentWatcher::Config.get.config[:notify_from],
+      mail( to: LaDepartmentWatcher::Config.get.config[:notify_to],
            subject: "Department #{department.name} is offline")
     end
   end
